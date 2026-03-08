756fd656ff84cd8d10d82e1031fd759da372a73d
Jereme Strange <ajsupplycollc@gmail.com>
2026-03-08 10:21:54 -0400

    Remove stray helper scripts from repo

--- check_token.ps1
+++
@@ -1,6 +0,0 @@
-$json = Get-Content 'C:\Users\ajsup\.openclaw\openclaw.json' -Raw | ConvertFrom-Json
-$token = $json.env.GITHUB_SAM_TOKEN
-$auth = "token " + $token
-$r = Invoke-WebRequest -Uri "https://api.github.com/user" -Headers @{Authorization=$auth}
-Write-Host "Scopes: $($r.Headers['X-OAuth-Scopes'])"
-Write-Host "Login: $(($r.Content | ConvertFrom-Json).login)"
